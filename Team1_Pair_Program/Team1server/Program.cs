a44a3e9637182cd27f9ad68d44a804f358ff6ec7
JFyoutheng <jfukuyama@youtheng.co.jp>
2026-06-05 16:10:12 +0900

    Visial Studio

---
+++ Team1_Pair_Program/Team1server/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Team1server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
